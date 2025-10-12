9c81496f5b6916e4e1ed88d4329fe3ae4ecabb59
Keroshi <bgathendu@live.com>
2025-10-12 22:11:19 +0300

    Modified settings.py

--- backend/Usalama_Wangu/settings.py
+++ backend/Usalama_Wangu/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+else:
+    ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
