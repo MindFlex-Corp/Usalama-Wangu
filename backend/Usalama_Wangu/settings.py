571d93b82f8d8cb11faf03f7a35ea4541ecb5a3a
Keroshi <bgathendu@live.com>
2025-10-12 21:57:38 +0300

    Modified backend

--- backend/Usalama_Wangu/settings.py
+++ backend/Usalama_Wangu/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!v^f&)_=90m#1i%=9ajy3^4^be5_9zp*=ge^678+fp5+4o!eu'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'backend.Alerts',
-    'backend.Zones',
-    'backend.api',
+    'Alerts',
+    'Zones',
+    'api',
 ]
 
 MIDDLEWARE = [
