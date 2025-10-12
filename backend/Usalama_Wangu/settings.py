7a8c5d23316976e78a14edb2b3441c47d671ecd2
Keroshi <bgathendu@live.com>
2025-10-12 21:43:57 +0300

    Modified app.py in all local apps

--- backend/Usalama_Wangu/settings.py
+++ backend/Usalama_Wangu/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Alerts',
-    'Zones',
-    'api',
+    'backend.Alerts',
+    'backend.Zones',
+    'backend.api',
 ]
 
 MIDDLEWARE = [
